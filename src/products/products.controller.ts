ea959040f01592e2d812980753cb847c4de88d06
Christian Lisangola <jochri3@gmail.com>
2022-07-31 22:22:55 +0300

    add create product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateCategoryDto } from "../categories/dtos/create-category.dto";
-import { CategoriesService } from "../categories/categories.service";
+import { CreateCategoryDto } from '../categories/dtos/create-category.dto';
+import { CategoriesService } from '../categories/categories.service';
+import { CreateProductDto } from './dtos/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private productService: ProductsService,private categoryService:CategoriesService) {}
+  constructor(
+    private productService: ProductsService,
+    private categoryService: CategoriesService,
+  ) {}
 
   @Get()
   findAll() {
@@ -13,7 +17,8 @@ export class ProductsController {
   }
 
   @Post()
-  async create(@Body() body:CreateCategoryDto){
-      const category=await this.categoryService;
+  async create(@Body() body: CreateProductDto) {
+    const category = await this.categoryService.findOne(body.categoryId);
+    return this.productService.create(body, category);
   }
 }
