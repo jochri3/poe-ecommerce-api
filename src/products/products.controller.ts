16a85f6976f653e87f760f87e9240219e44164c7
Christian Lisangola <jochri3@gmail.com>
2022-07-31 22:15:35 +0300

    add findAll products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { ProductsService } from './products.service';
+import { CreateCategoryDto } from "../categories/dtos/create-category.dto";
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    return this.productService.findAll();
+  }
+
+  @Post()
+  create(@Body() body:CreateCategoryDto){
+
+  }
+}
